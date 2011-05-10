8e944419acc0eed85aa252b24e7456d7b2f5a7d7
martin chan <osiutino@gmail.com>
2011-05-10 16:01:28 +0800

    rearrange the code in main.c

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ void high_ISR(void){
 	if(PIR1bits.ADIF == 1)
 	{
 		if(ReadADC() <200 )
-			PORTCbits.RC4 = ~PORTCbits.RC4;
+			LI_SIGN_LED = ~LI_SIGN_LED;
 		PIR1bits.ADIF = 0;
 	}
 //	if(PIR1bits.TMR1IF == 1) {
@@ -24,45 +24,43 @@ void high_ISR(void){
 //	}
 }
 /////////////////////////////////////
-
+#pragma udata mydata
+ unsigned char buffer1[6],buffer2[6];
+ char astr[60]; 
+#pragma
 
 void main(void){
-	
-	unsigned char buffer1[6],buffer2[6];
-	char astr[60];
-	//char startstr[] = "\n\r start > \n";
-	//char to232[] = "To RS232\r\n";
-	//char toxbee[] = "To XBee\r\n";
 	int i = 0,adcResult;
 	unsigned char error, checksum;
 	value humi_val, temp_val;
 	myfloat h_val , t_val;
 
+	UART_CTS_TRIS = OUT;	
+	UART_TX_TRIS = OUT;		 //Tx
+	UART_RX_TRIS = IN;		//Rx
+	
+	CUART_TX_TRIS = OUT;	//TX
+	CUART_RX_TRIS = IN;		//RX
+
+	LI_SIGN_LED_TRIS = OUT;
 
 	SHT10_CONN_LED_TRIS = OUT;
 	SHT10_SIGN_LED_TRIS = OUT;
 	SHT10_CONN_LED = 0;
 	SHT10_SIGN_LED = 0;
 
-	UART_CTS_TRIS = OUT;	
-	UART_TX_TRIS = OUT; //Tx
-	UART_RX_TRIS = IN;	//Rx
-	
-	TRISCbits.TRISC4 = OUT;
-
 	OpenADC(ADC_FOSC_8 & ADC_RIGHT_JUST , ADC_CH0 & ADC_INT_ON , 0);
-
-
-	ADCON1 = 0x0d;					//All digital operation	for RA2 & RA0 pins being digital for SHT10 sensor
-
 	OpenUSART(USART_TX_INT_OFF & USART_RX_INT_ON & USART_ASYNCH_MODE &
-			  USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH ,129);
-	
+			  USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH ,BAUD_RATE_GEN);
 
+	s_softreset();			//reset the SHT10 sensor
+	cUSART_tInit();
+
+	ADCON1 = 0x0d;			//All digital operation	for RA2 & RA0 pins being digital for SHT10 sensor
 	RCONbits.IPEN = 1; 		//enable interrupt priority
-	IPR1bits.ADIP = 1;			// A/D high interrupt priority 
-	PIR1bits.ADIF = 0;			// A/D converter interrupt flag bit clear
-	PIE1bits.ADIE = 1;			// A/D interrupt enabled
+	IPR1bits.ADIP = 1;		// A/D high interrupt priority 
+	PIR1bits.ADIF = 0;		// A/D converter interrupt flag bit clear
+	PIE1bits.ADIE = 1;		// A/D interrupt enabled
 	
 	IPR1bits.TMR1IP = 1;	//set TMR1 interrupt to high priorit
 
@@ -71,18 +69,9 @@ void main(void){
 	PIR1bits.RCIF = 0;		//PIR1bits.RCIF
 	PIE1bits.RCIE = 1;		//Reception interrupt Enable
 	INTCONbits.GIEH = 1;	//enable all interrupts
-
-	s_softreset();			//reset the SHT10 sensor
-
-	//while(BusyUSART());
-	//putsUSART(startstr);
-	
-	cUSART_tInit();
-
-	CUART_TX_TRIS = OUT;	//TX
-	CUART_RX_TRIS = IN;		//RX
 	
 	while(1){
+
 		error=0;
 	    error+=s_measure((unsigned char*)&humi_val.i,&checksum,HUMI);  	//measure humidity
 	    error+=s_measure((unsigned char*)&temp_val.i,&checksum,TEMP);  	//measure temperature
